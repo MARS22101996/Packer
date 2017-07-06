a22a53103c4b89945e9351ae75addccd196259b3
Mariia Suvalova <mariia_suvalova@epam.com>
2017-07-06 20:47:29 +0300

    stack

--- remove_services.sh
+++ remove_services.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-docker service remove poc_team
-docker service remove poc_ticket
-docker service remove poc_notification
-docker service remove poc_statistic
-docker service remove poc_user
-docker service remove poc_api-gateway
-docker service remove poc_ui
-docker service remove poc_mongodb
-docker service remove poc_fabio
+sudo docker stack rm
