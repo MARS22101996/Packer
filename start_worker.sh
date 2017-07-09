0651939809250001cf94afa1b09788ac33c0b65c
Mariia Suvalova <mariia_suvalova@epam.com>
2017-07-09 13:24:48 +0300

    IP

--- start_worker.sh
+++ start_worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #You should change tocken and IP for appropriate SWARM manager
-sudo docker swarm join --token SWMTKN-1-37v2vqo82mgtgo0roy7gtffo5lo92f5yf68f9vduvxgm516t3u-cpjwhzxnbtc3vwtidwc5vsy4w 10.23.22.40:2377
+sudo docker swarm join --token SWMTKN-1-37v2vqo82mgtgo0roy7gtffo5lo92f5yf68f9vduvxgm516t3u-cpjwhzxnbtc3vwtidwc5vsy4w 192.168.56.101:2377
