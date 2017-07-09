9b86d9b8e7bb32de1f77d0e018421e3da3bba28f
Mariia Suvalova <mariia_suvalova@epam.com>
2017-07-09 14:56:47 +0300

    IP

--- start_worker.sh
+++ start_worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #You should change token (copy file with tocken) and IP for appropriate SWARM manager
-docker swarm join --token $(cat ./worker_token) 192.168.56.102:2377
+docker swarm join --token $(cat ./worker_token) 10.23.20.56:2377
