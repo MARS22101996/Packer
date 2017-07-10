97ce23f8c945e957f179761d8eddb6f722774d77
Mariia Suvalova <mariia_suvalova@epam.com>
2017-07-10 21:49:05 +0300

    Change path

--- pull_services.sh
+++ pull_services.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-git init
+sudo git init
 
-git config user.email "maria_suvalova@mail.ru"
+sudo git config user.email "maria_suvalova@mail.ru"
 
-git config user.name "Mary Suvalova"
+sudo git config user.name "Mary Suvalova"
 
-git pull https://user:riama2210@github.com/MARS22101996/Packer.git master
+sudo git pull https://user:riama2210@github.com/MARS22101996/Packer.git master
 
 cd ./services
 
